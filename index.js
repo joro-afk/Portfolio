545760026b69f5c29b46eefc0d3296dc4915aa7b
Joaquin <91098938+joro-afk@users.noreply.github.com>
2023-03-25 21:34:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,22 @@ window.onscroll = function () {
     window.open("https://joro-afk-sony.netlify.app/");
   }
 
-
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_cq1el85", "template_4kdp6tb", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
   
