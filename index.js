be794bae6d55f7c88bfd19294fc02268c4c98522
Joa <91098938+joro-afk@users.noreply.github.com>
2023-06-10 00:17:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-
-
-
-
 window.onscroll = function () {
-    myFunction();
-  };
-  var navbar = document.getElementById("principal");
-  
-  var sticky = navbar.offsetTop;
-  
-  function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-    }
+  myFunction();
+};
+var navbar = document.getElementById("principal");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
   }
-  
-  
-  document.getElementById("left").addEventListener("click", weblink);
-  
-  function weblink() {
-    window.open("https://joro-afk-sony.netlify.app/");
-  }
+}
 
 window.onload = function () {
   document
     .getElementById("contact-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
-      // generate a five digit number for the contact_number variable
       this.contact_number.value = (Math.random() * 100000) | 0;
-      // these IDs from the previous steps
       emailjs.sendForm("service_cq1el85", "template_4kdp6tb", this).then(
         function () {
           console.log("SUCCESS!");
@@ -42,4 +29,3 @@ window.onload = function () {
       );
     });
 };
-  
