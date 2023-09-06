2020e9a5581c6edce8332b3eb05b7733a2451db4
Joa <91098938+joro-afk@users.noreply.github.com>
2023-09-06 21:03:46 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-window.onscroll = function () {
-  myFunction();
-};
-var navbar = document.getElementById("principal");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 window.onload = function () {
   document
